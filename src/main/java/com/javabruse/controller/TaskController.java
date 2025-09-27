7ca04f08916db35e49a2e2138f89e1ef99681f5d
FiataliS <FiataliS@gmail.com>
2025-09-27 11:27:19 +0500

    update code from task message to kafka

--- src/main/java/com/javabruse/controller/TaskController.java
+++ src/main/java/com/javabruse/controller/TaskController.java
@@ -4,16 +4,17 @@ import com.javabruse.DTO.CamMessage;
 import com.javabruse.DTO.ConstructionMessage;
 import com.javabruse.DTO.PhotoMessage;
 import com.javabruse.DTO.TaskMessage;
+import com.javabruse.model.Task;
+import com.javabruse.service.TaskService;
 import com.javabruse.service.kafka.KafkaProducerService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,42 +28,105 @@ public class TaskController {
 
     private final KafkaProducerService kafkaProducerService;
     private static final Logger log = LoggerFactory.getLogger(TaskController.class);
+    private final TaskService taskService;
 
-
-    @GetMapping("/send-task")
-    public ResponseEntity<Object> getAllPostBySourceID(HttpServletRequest request) {
+    @Operation(summary = "Получить все задачи пользователя")
+    @GetMapping("/all")
+    public ResponseEntity<Object> getAll(HttpServletRequest request) {
         UUID userUUID = UUID.fromString(request.getHeader("X-User-Id"));
-        log.info("UUID пользователя: " + userUUID);
         try {
-            TaskMessage message = new TaskMessage();
-            message.setId(UUID.randomUUID());
-            message.setTaskID(UUID.randomUUID());
-            PhotoMessage photoMessage = new PhotoMessage();
-            CamMessage camMessage = new CamMessage();
-            camMessage.setElevation(4156d);
-            camMessage.setId(UUID.randomUUID());
-            camMessage.setBearing(4564d);
-            camMessage.setLongitude(45615646d);
-            camMessage.setLatitude(3123412d);
-            camMessage.setAddress("Адрес камеры");
-            photoMessage.setFilePath("/3123/5125/fweg/213/Какой то путь/");
-            photoMessage.setId(UUID.randomUUID());
-            List<ConstructionMessage> list = new ArrayList<>();
-            for (int i = 0; i < 5; i++) {
-                ConstructionMessage constructionMessage = new ConstructionMessage();
-                constructionMessage.setLongitude(Double.valueOf(((i + 12) * 5161)));
-                constructionMessage.setLatitude(Double.valueOf(((i + 32) * 3123)));
-                constructionMessage.setAddress("Адресс" + i + 2);
-                list.add(constructionMessage);
-            }
-            photoMessage.setConstructionMessageList(list);
-            photoMessage.setCamMessage(camMessage);
-            message.setPhotoMessage(photoMessage);
-            kafkaProducerService.sendTransferRequestTask(message);
-            return ResponseEntity.status(HttpStatus.OK).body("Отправлено!");
+            return ResponseEntity.status(HttpStatus.OK).body(taskService.getAll(userUUID));
         } catch (Exception e) {
-            log.error("/v1/task/send-task    Error -----------------------------" + e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
+    @Operation(summary = "Добавляет новую задачу")
+    @PostMapping("/add")
+    public ResponseEntity<Object> add(@RequestBody Task task, HttpServletRequest request) {
+        UUID userUUID = UUID.fromString(request.getHeader("X-User-Id"));
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(taskService.add(task, userUUID));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @Operation(summary = "Обновляет новую задачу")
+    @PostMapping("/save")
+    public ResponseEntity<Object> save(@RequestBody Task task, HttpServletRequest request) {
+        UUID userUUID = UUID.fromString(request.getHeader("X-User-Id"));
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(taskService.update(task, userUUID));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @Operation(summary = "Удаляет задачу по ID")
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Object> deleteFromIdByUUID(@PathVariable String id, HttpServletRequest request) {
+        UUID userUUID = UUID.fromString(request.getHeader("X-User-Id"));
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(taskService.delete(UUID.fromString(id), userUUID));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @GetMapping("/send-task")
+//    public ResponseEntity<Object> getAllPostBySourceID(HttpServletRequest request) {
+//        UUID userUUID = UUID.fromString(request.getHeader("X-User-Id"));
+//        log.info("UUID пользователя: " + userUUID);
+//        try {
+//            TaskMessage message = new TaskMessage();
+//            message.setId(UUID.randomUUID());
+//            message.setTaskID(UUID.randomUUID());
+//            PhotoMessage photoMessage = new PhotoMessage();
+//            CamMessage camMessage = new CamMessage();
+//            camMessage.setElevation(4156d);
+//            camMessage.setId(UUID.randomUUID());
+//            camMessage.setBearing(4564d);
+//            camMessage.setLongitude(45615646d);
+//            camMessage.setLatitude(3123412d);
+//            camMessage.setAddress("Адрес камеры");
+//            photoMessage.setFilePath("/3123/5125/fweg/213/Какой то путь/");
+//            photoMessage.setId(UUID.randomUUID());
+//            List<ConstructionMessage> list = new ArrayList<>();
+//            for (int i = 0; i < 5; i++) {
+//                ConstructionMessage constructionMessage = new ConstructionMessage();
+//                constructionMessage.setLongitude(Double.valueOf(((i + 12) * 5161)));
+//                constructionMessage.setLatitude(Double.valueOf(((i + 32) * 3123)));
+//                constructionMessage.setAddress("Адресс" + i + 2);
+//                list.add(constructionMessage);
+//            }
+//            photoMessage.setConstructionMessageList(list);
+//            photoMessage.setCamMessage(camMessage);
+//            message.setPhotoMessage(photoMessage);
+//            kafkaProducerService.sendTransferRequestTask(message);
+//            return ResponseEntity.status(HttpStatus.OK).body("Отправлено!");
+//        } catch (Exception e) {
+//            log.error("/v1/task/send-task    Error -----------------------------" + e.getMessage());
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//        }
+//    }
 }
