3c4ba84e7ad86e1cf04b4b82cbc52be05493c3de
FiataliS <FiataliS@gmail.com>
2025-09-27 13:48:24 +0500

    update code from task message to kafka

--- src/main/java/com/javabruse/Main.java
+++ src/main/java/com/javabruse/Main.java
@@ -3,6 +3,7 @@ package com.javabruse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = {
         "com.javabruse.model",
 })
+@ComponentScan(basePackages = "com.javabruse")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
